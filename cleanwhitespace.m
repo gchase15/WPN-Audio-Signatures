6febc433ebefaae4b008ab36e0f6bbd98d99265e
Kristen V Gasser <60211484+kristenvgasser@users.noreply.github.com>
2020-02-23 21:33:26 -0700

    Initial Commot of Script to clean whitespace out of sound files

---
+++ cleanwhitespace.m
@@ -0,0 +1,40 @@
+clear all; close all; clc;
+% load 'Gavin_WeaponSounds/AK47/*.wav';
+
+files = dir('Gavin_WeaponSounds/AK47/*.wav') ; 
+
+
+minWhitespace = 5;
+N = length(files);   
+for i = 1:N
+    test = [];
+    consecutiveWhitespace = [];
+    thisfile = files(i).name ; 
+    [data,fs] = audioread(thisfile);
+%     sound(data, fs);
+    datalength = length(data);
+    for j = 1:datalength
+        if (j > datalength)
+            break;
+        end
+        if (data(j,1) ~= 0)
+            consecutiveWhitespace = [];
+            continue;
+        else
+            consecutiveWhitespace = [consecutiveWhitespace data(j,1)];
+        end
+        whitespacesize = size(consecutiveWhitespace,2);
+        if (whitespacesize == minWhitespace)
+            for k=1:whitespacesize
+                data(j+1-k,:) = [];
+                test = [test data(j+1-k,1)];
+                datalength = datalength - 1;
+            end
+            consecutiveWhitespace = [];
+        end
+    end
+    newFileName = split(thisfile,".");
+    filename = string(newFileName(1,1));
+    newFileNameChar = filename + '_Cleaned.wav';
+    audiowrite(newFileNameChar, data, fs);
+ end
